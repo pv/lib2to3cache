5c47f9d5bc00e6989338b0c3c0dcd49be35fca15
Pauli Virtanen <pav@iki.fi>
2010-07-18 15:35:10 +0200

    Add script back

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
-import setuptools
+try:
+    import setuptools
+    kw = dict(entry_points=dict(
+        console_scripts=['2to3cache = lib2to3cache:main']))
+except ImportError:
+    kw = dict(scripts=["2to3cache"])
+
 from distutils.core import setup
 
 setup(name='lib2to3cache',
       version='0.1',
       py_modules=['lib2to3cache'],
-      entry_points = dict(console_scripts=['2to3cache = lib2to3cache:main']),
-      )
+      **kw)
