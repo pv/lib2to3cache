8a1148ab04359f9b56f57b8d98a37246e976f9fc
Pauli Virtanen <pav@iki.fi>
2010-07-18 14:42:41 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='lib2to3cache',
+      version='0.1',
+      py_modules=['lib2to3cache'],
+      scripts=['2to3cache'],
+      )
